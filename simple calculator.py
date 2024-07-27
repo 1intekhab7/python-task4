db2a1de90e987180d0030b498c0f125d3a559b43
1intekhab7 <ia5931955@gmail.com>
2024-07-27 21:28:49 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,31 @@
+#pyhon program for simple calculator 
+
+#function to add two numbers
+def add(num1, num2):
+    return num1 + num2
+
+
+#function to subtract two numbers 
+ def subtract(num1, num2): 
+     return num1 - num2
+
+
+#function to multiply two numbers
+ def multiply(num1, num2):
+    return num1 * num2
+
+
+#function to divide two numbers
+ def divide(num1, num2):
+     return num1 / num2
+
+
+print("please select operation -\n" \
+      "1. add\n"  \
+      "2. subtract\n"  \
+      "3. multiply\n"  \
+      "4 divide\n")
+
+
+
+   
